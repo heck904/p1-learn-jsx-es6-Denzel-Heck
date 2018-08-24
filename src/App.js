89d614378ec564276b0c14e9f311367b4df8c782
heck904 <ddheck@valdosta.edu>
2018-08-24 01:59:48 -0400

    promises

--- src/App.js
+++ src/App.js
@@ -149,6 +149,27 @@ console.log(`Example 7: Spread Operator`)
     console.log(`Minimum Integer: `, minNum);
     console.log(`----------------------`)
 
+    //Example 5: Promises
+    console.log(`Example 5: Promises`)
+    console.log(`Had to be put at the end`)
+    const getFakeMembers = count => new Promise((resolves, rejects) => {
+      const api = `https://api.randomuser.me/?nat=US&results=${count}`
+      const request = new XMLHttpRequest()  
+      request.open('GET', api)  
+      request.onload = () => (request.status === 200) ? 
+      resolves(JSON.parse(request.response).results) : 
+      rejects(Error(request.statusText))  
+      request.onerror = (err) => rejects(err)  })
+
+    getFakeMembers(5).then(  
+      members => console.log(members),  
+      err => console.error(
+        new Error("cannot load members from randomuser.me")))
+
+
+
+
+
 
       return (
       <div className="App">
